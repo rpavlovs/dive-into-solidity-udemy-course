4637cc183ec8d4cef9bcd8a50a7124528f745682
Roman Pavlovskyi <rpavlovskyi@gmail.com>
2022-08-04 21:05:32 -0700

    Complete the knowledge test

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -6,8 +6,46 @@ contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
+    }
+
+    function changeTokens() public {
+        string[] storage t = tokens;
         t[0] = "VET";
+    }
+
+    receive() external payable {
+        // Do nothing
+    }
+
+    fallback() external payable {
+        // Do nothing
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function transferAll(address payable _to) public onlyOwner {
+        uint balance = address(this).balance;
+        (bool success, bytes memory returnBytes) = _to.call{value: balance}('');
+        (success, returnBytes);
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string calldata a, string calldata b) public pure returns (string memory) {
+        string memory ab = string.concat(a, b);
+        return ab;
     }
 }
