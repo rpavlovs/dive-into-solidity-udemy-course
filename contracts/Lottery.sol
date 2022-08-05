eba164ac1752ece0fa77c01d460b4f351e10f1de
Roman Pavlovskyi <rpavlovskyi@gmail.com>
2022-08-04 21:26:08 -0700

    Complete lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,37 +11,38 @@ contract Lottery {
 
     // declaring the constructor
     constructor() {
-        // TODO: initialize the owner to the address that deploys the contract
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
-        // TODO: require each player to send exactly 0.1 ETH
-        // TODO: append the new player to the players array
+        require(msg.value == 0.1 ether);
+        players.push(msg.sender);
     }
 
     // returning the contract's balance in wei
-    function getBalance() public view returns (uint256) {
-        // TODO: restrict this function so only the owner is allowed to call it
-        // TODO: return the balance of this address
+    function getBalance() public view onlyOwner returns (uint256) {
+        return address(this).balance;
     }
 
     // selecting the winner
-    function pickWinner() public {
-        // TODO: only the owner can pick a winner 
-        // TODO: owner can only pick a winner if there are at least 3 players in the lottery
+    function pickWinner() public onlyOwner {
+        require(players.length > 3, 'NOT_ENOUGH_PLAYERS');
 
         uint256 r = random();
-        address winner;
+        address winner = players[r % players.length];
+        gameWinners.push(winner);
 
-        // TODO: compute an unsafe random index of the array and assign it to the winner variable 
-
-        // TODO: append the winner to the gameWinners array
-
-        // TODO: reset the lottery for the next round
-
-        // TODO: transfer the entire contract's balance to the winner
+        players = new address[](0);
+        (bool success, ) = winner.call{ value: address(this).balance }('');
+        require(success, 'Failed to send funds');
     }
+
 
     // helper function that returns a big random integer
     // UNSAFE! Don't trust random numbers generated on-chain, they can be exploited! This method is used here for simplicity
